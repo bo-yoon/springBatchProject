0343f40c4cbe9916ff4332e62abea463cef491b2
bo-yoon <bykown223@gmail.com>
2021-05-10 13:46:29 +0900

    init

--- jpashop/src/main/java/jpashop/domain/DormantStatus.java
+++
@@ -1,5 +0,0 @@
-package jpashop.domain;
-
-public enum DormantStatus {
-	ACTIVE, SLEEP
-}
--- jpashop/src/main/java/jpashop/domain/coupon/CouponStatus.java
+++
@@ -1,7 +0,0 @@
-package jpashop.domain.coupon;
-
-
-// 배치 프로그램 작동시 기간이 지난 쿠폰은 EXPIRATION으로 변경 
-public enum CouponStatus {
-	UNUSED, USED, EXPIRATION
-}
